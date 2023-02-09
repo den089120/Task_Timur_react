6c2de610049560421c9f5015f9ae6752318ae2ba
den089120 <den089120@mail.ru>
2023-02-09 12:05:23 +0700

    add update project 7

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -17,5 +17,6 @@ export function buildPlugins({paths, isDev}: BuildOptions):webpack.WebpackPlugin
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev)
         }),
+        new webpack.HotModuleReplacementPlugin(),
     ]
 }
